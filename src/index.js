eb9c20a096eb148057969ad80493b73a1beb7028
Belhassen <152809495+BelhassenB@users.noreply.github.com>
2024-06-06 16:30:37 +0200

    Add IIFE to load pages

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import './style.css';
 import RestaurantImg from './img/my-restau-image.jpg'
-import {divContent, displayHomeContent, dispalyHomeContentOnClick, homeContent} from './home';
-import { displayMenu } from './menu';
+import { homeBtn, divContent, homePage } from './home';
+import { menuBtn, menuPage } from './menu';
+import { aboutBtn, aboutPage } from './about';
 
-displayHomeContent()
-dispalyHomeContentOnClick()
-displayMenu()
+const displayHome = ( () => {
+    homeBtn.addEventListener("click", homePage)
+    
+})()
+const displayHomeOnLoad = (() => {
+    document.addEventListener("DOMContentLoaded", homePage)
+})()
 
+const displayMenu = ( () => {
+    menuBtn.addEventListener("click", menuPage )
+})()
+
+const displayAbout = (() => {
+    aboutBtn.addEventListener("click", aboutPage)
+})()
